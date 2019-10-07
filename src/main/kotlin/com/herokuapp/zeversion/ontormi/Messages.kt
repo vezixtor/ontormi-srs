60794f2671a8d62a1c2502d2edaa0d455b53f621
Everson <everson___@hotmail.com>
2019-10-06 21:04:25 -0300

    add messages

---
+++ src/main/kotlin/com/herokuapp/zeversion/ontormi/Messages.kt
@@ -0,0 +1,11 @@
+package com.herokuapp.zeversion.ontormi
+
+// USER MESSAGES
+const val USER_NAME_NOT_BLANK = "User name can not be blank"
+const val USER_EMAIL_NOT_BLANK = "User email can not be blank"
+const val USER_EMAIL_INVALID = "User email must be valid"
+const val USER_NOT_EXISTS = "None user with requested ID was found"
+
+// STUDY MESSAGES
+const val STUDY_SUBJECT_NOT_BLANK = "Study subject could not be blank"
+const val STUDY_DESCRIPTION_BLANK = "Study description could not be blank"
